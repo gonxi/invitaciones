203116a82243583b314574bbe4916185c7743704
gonxi <gonxi.gonzalo@gmail.com>
2015-03-30 16:27:05 +0200

    vista delete

--- recall.rb
+++ recall.rb
@@ -34,3 +34,22 @@ get '/:id' do
 	@title = "Edit note ##{params[:id]}"
 	erb :edit
 end
+
+put '/:id' do
+  n = Note.get params[:id]
+  n.content = params[:content]
+  n.complete = params[:complete] ? 1 : 0
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
+get '/:id/delete' do
+  @note = Note.get params[:id]
+  @title = "Confirm deletion of note ##{params[:id]}"
+  erb :delete
+end
+delete '/:id' do
+  n = Note.get params[:id]
+  n.destroy
+  redirect '/'
+end
