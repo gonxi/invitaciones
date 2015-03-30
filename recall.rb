8d01dbecfc2316063f71ef1d629f055b8f8cc74a
gonxi <gonxi.gonzalo@gmail.com>
2015-03-30 12:54:55 -0700

    Sustituyo Note por Event en recall.rb y creo BBDD

--- recall.rb
+++ recall.rb
@@ -1,14 +1,14 @@
 require 'sinatra'
 require 'data_mapper'
 
-DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/recall.db")
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/appevents.db")
 
 class Event
 	include DataMapper::Resource
 	property :id, Serial
 	property :title, Text, :required => true
 	property :content, Text, :required => true
-	property :date, DateTime :required=> true
+	property :date_event, DateTime, :required => true
 	property :created_at, DateTime
 	property :updated_at, DateTime
 end
@@ -22,45 +22,47 @@ get '/' do
 end
 
 post '/' do
-	n = Event.new
-	n.content = params[:content]
-	
-	n.created_at = Time.now
-	n.updated_at = Time.now
-	n.save
+	e = Event.new
+	e.title = params[:title]
+	e.content = params[:content]
+	e.date = params [:date_event]
+	e.created_at = Time.now
+	e.updated_at = Time.now
+	e.save
 	redirect '/'
 end
 
 get '/:id' do
-	@note = Note.get params[:id]
-	@title = "Edit note ##{params[:id]}"
+	@note = Event.get params[:id]
+	@title = "Editar evento ##{params[:id]}"
 	erb :edit
 end
 
 put '/:id' do
-  n = Note.get params[:id]
-  n.content = params[:content]
-  n.complete = params[:complete] ? 1 : 0
-  n.updated_at = Time.now
-  n.save
+  e = Event.get params[:id]
+  e.title = paramas[:title]
+  e.date = paramas[:date_event]
+  e.content = params[:content]
+  e.updated_at = Time.now
+  e.save
   redirect '/'
 end
 get '/:id/delete' do
-  @note = Note.get params[:id]
-  @title = "Confirm deletion of note ##{params[:id]}"
+  @note = Event.get params[:id]
+  @title = "Confirma que quiere eiliminar el evento ##{params[:id]}"
   erb :delete
 end
 
 delete '/:id' do
-  n = Note.get params[:id]
-  n.destroy
+  e = Event.get params[:id]
+  e.destroy
   redirect '/'
 end
 
 get '/:id/complete' do
-  n = Note.get params[:id]
-  n.complete = n.complete ? 0 : 1 # flip it
-  n.updated_at = Time.now
-  n.save
+  e = Event.get params[:id]
+  e.complete = n.complete ? 0 : 1 # flip it
+  e.updated_at = Time.now
+  e.save
   redirect '/'
 end
