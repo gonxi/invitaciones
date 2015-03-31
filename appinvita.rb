e1627c48df5a6bc42404011a146f5f67f04d9a23
gonxi <gonxi.gonzalo@gmail.com>
2015-03-31 16:07:21 -0700

    Creo index, y separo la parte de crear

--- appinvita.rb
+++ appinvita.rb
@@ -27,15 +27,31 @@ end
 DataMapper.finalize.auto_upgrade!
 
 
-#***********************INSERT*****************************
+#***********************INDEX****************************
 
-get '/' do 
+get '/' do
+
+@title = 'Hola! Este es el proyecto'
+erb :index
+
+end
+
+#******************
+
+get '/events' do 
 	@events = Event.all :order => :id.desc
 	@title = 'Todos los Eventos'
 	erb :home
 end
 
-post '/' do
+
+get '/create' do
+	@title = 'Crear Eventos'
+	erb :create
+end
+
+
+post '/create' do
 	e = Event.new
 	e.title = params[:title]
 	e.content = params[:content]
@@ -43,51 +59,51 @@ post '/' do
 	e.created_at = Time.now
 	e.updated_at = Time.now
 	e.save
-	redirect '/'
+	redirect '/events'
 end
 
 #*************************EDIT***************************
 
-get '/:id' do
+get '/events/:id' do
 	@event = Event.get params[:id]
 	@title = "Editar evento ##{params[:id]}"
 	erb :edit
 end
 
-put '/:id' do
+put '/events/:id' do
   e = Event.get params[:id]
   e.title = params[:title]
   e.date_event = params[:date_event]
   e.content = params[:content]
   e.updated_at = Time.now
   e.save
-  redirect '/'
+  redirect '/events'
 end
 
 #********************DELETE***************************
 
-get '/:id/delete' do
+get '/events/:id/delete' do
   @event = Event.get params[:id]
   @title = "Confirma que quiere eiliminar el evento ##{params[:id]}"
   erb :delete
 end
 
-delete '/:id' do
+delete '/events/:id' do
   e = Event.get params[:id]
   e.destroy
-  redirect '/'
+  redirect '/events'
 end
 
 #**********************INVITE***********************
 
-get '/:id/invite' do
+get '/events/:id/invite' do
 	@title = "Hacer la invitacion a un evento"
 	@event = Event.get params[:id]
-	@invites = Invite.all(:id_event => @event.id) 
+	@invites = Invite.all(:id_event => @event.id ) 
 	erb :invite
 end
 
-post '/:id/invite' do
+post '/events/:id/invite' do
 	e = Event.get params[:id]
 	i = Invite.new
 	i.id_event = e.id
@@ -95,10 +111,9 @@ post '/:id/invite' do
 	i.name_invited = params[:name_invited]
 	i.created_at = Time.Now
 	i.save
-	redirect '/'
 end
 
-put '/:id/invite' do
+put '/events/:id/invite' do
 	e = Event.get params[:id]
 	i = Invite.new
 	i.id_event = e.id
@@ -106,7 +121,8 @@ put '/:id/invite' do
 	i.name_invited = params[:name_invited]
 	i.created_at = Time.now
 	i.save
-	redirect '/'
+	redirect '/events'
+
 end
 
 
