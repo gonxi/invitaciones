8b0cda5d0b45db6d0816b39e14dad0b4adbaa20d
gonxi <gonxi.gonzalo@gmail.com>
2015-03-31 12:58:16 -0700

    Ya filtro las invitaciones por cada evento)

--- appinvita.rb
+++ appinvita.rb
@@ -3,6 +3,8 @@ require 'data_mapper'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/appevents.db")
 
+#**********CLASES BBDD****************
+
 class Event
 	include DataMapper::Resource
 	property :id, Serial
@@ -17,12 +19,15 @@ class Invite
 	include DataMapper::Resource
 	property :id, Serial
 	property :id_event, Text
+	property :name_invited, Text
 	property :email, Text, :required => true 
 	property :created_at, DateTime
 end
 
 DataMapper.finalize.auto_upgrade!
 
+
+#***********************INSERT*****************************
 
 get '/' do 
 	@events = Event.all :order => :id.desc
@@ -41,6 +46,8 @@ post '/' do
 	redirect '/'
 end
 
+#*************************EDIT***************************
+
 get '/:id' do
 	@event = Event.get params[:id]
 	@title = "Editar evento ##{params[:id]}"
@@ -57,6 +64,8 @@ put '/:id' do
   redirect '/'
 end
 
+#********************DELETE***************************
+
 get '/:id/delete' do
   @event = Event.get params[:id]
   @title = "Confirma que quiere eiliminar el evento ##{params[:id]}"
@@ -69,17 +78,35 @@ delete '/:id' do
   redirect '/'
 end
 
+#**********************INVITE***********************
+
 get '/:id/invite' do
-	@title = "HAcer la invitacion a un evento"
+	@title = "Hacer la invitacion a un evento"
 	@event = Event.get params[:id]
+	@invites = Invite.all(:id_event => @event.id) 
 	erb :invite
 end
 
 post '/:id/invite' do
+	e = Event.get params[:id]
 	i = Invite.new
-	i.id_event = params[:id_event]
+	i.id_event = e.id
 	i.email = params[:email]
+	i.name_invited = params[:name_invited]
 	i.created_at = Time.Now
+	i.save
+	redirect '/'
+end
+
+put '/:id/invite' do
+	e = Event.get params[:id]
+	i = Invite.new
+	i.id_event = e.id
+	i.email = params[:email]
+	i.name_invited = params[:name_invited]
+	i.created_at = Time.now
+	i.save
+	redirect '/'
 end
 
 
